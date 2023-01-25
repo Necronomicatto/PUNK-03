126311565bb30a544d25997d1f7e1a252349f2d4
California <94809867+P0W-P0W@users.noreply.github.com>
2023-01-25 17:40:04 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -11,9 +11,12 @@ int main(int argc, char *argv[]){
     in = fopen(argv[1], "r");
     out = fopen(argv[2],"w");
 
+    parameterTest(argc);
+    OpeningTest(in, argv[1], "r");
+
     readMatrix(Tabuleiro, in);      //chama função que lê a matriz do arquivo e passa pra variavel
     games(Tabuleiro, 'O', out);     //chama a função que faz os jogos
-    
+
     fclose(in); 
     fclose(out);
     return 0;
